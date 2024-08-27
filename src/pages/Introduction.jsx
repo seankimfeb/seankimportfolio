bdbc3f86caf2ed4349e6f3b949da5982aba3759b
Sean Kim <seankimfeb@gmail.com>
2024-08-27 16:39:49 -0400

    added contacts

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -3,7 +3,8 @@ import profileImage from '../components/images/CK0A3325.JPG';
 import carpoolImage from '../components/images/carpoolimage.jpg';
 import shoeImage from '../components/images/shoeimage.jpg';
 import carstoreImage from '../components/images/carstore.png';
-
+import githubimage from '../components/images/githublogo.png';
+import ldimage from  '../components/images/LinkedIn_logo_initials.png';
 const Introduction = () => {
     return (
         <div className="p-6 bg-gray-100 rounded-lg shadow-md mt-10">
@@ -12,13 +13,25 @@ const Introduction = () => {
                     <img src={profileImage} alt="Profile" className="rounded-full w-32 md:w-48" />
                 </div>
                 <div className="md:w-2/3 text-center md:text-left">
-                <p className="text-3xl md:text-5xl text-gray-700">
-                    Seong Su Kim
-                    </p>
-                    <p className="text-lg md:text-2xl text-gray-600 mt-2">
-                        Computer Science Student | Passionate about Web Development 
-                        </p>
+                    <p className="text-3xl md:text-5xl text-gray-700">Seong Su Kim</p>
+                    <p className="text-base md:text-xl text-gray-600 mt-2">A passionate Computer Science Student</p>
+                    <p className="text-lg md:text-2xl text-gray-600 mt-2">Proficient in Java, JavaScript, and Python | Specializing in Full-Stack Development</p>
                     
+                    {/* GitHub Profile Link */}
+                    <div className="flex items-center justify-center md:justify-start mt-4">
+                        <a href="https://github.com/seankimfeb" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-700 hover:text-blue-500 mr-4">
+                            <img src={githubimage} alt="GitHub" className="h-6 w-6 mr-2" />
+                            <span className="text-lg">GitHub</span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/seong-su-kim-642895229/" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-700 hover:text-blue-500">
+                            <img src={ldimage} alt="GitHub" className="h-6 w-6 mr-2" />
+                            <span className="text-lg">LinkedIn</span>
+                        </a>
+                    </div>
+                    <div className="mt-4 text-gray-700">
+        <span className="text-lg">Email: </span>
+        <a href="mailto:your.email@example.com" className="text-blue-500 hover:underline">seankimfeb@gmail.com</a>
+    </div>
                 </div>
             </div>
             <div className="mt-10">
@@ -27,9 +40,7 @@ const Introduction = () => {
                     <div className="bg-white p-6 rounded-lg shadow-md min-h-[400px] flex flex-col">
                         <img src={carstoreImage} alt="Project 1" className="rounded-lg mb-4 w-full h-48 object-cover" />
                         <h3 className="text-xl font-semibold mb-2">VehicleExpo E-commerce</h3>
-                        <p className="text-gray-600">A full-stack car merchandise web application.
-                            Has all functioning e-commerce components including login, review writing, checkout...etc.
-                        </p>
+                        <p className="text-gray-600">A full-stack car merchandise web application. Has all functioning e-commerce components including login, review writing, checkout...etc.</p>
                     </div>
                     <div className="bg-white p-6 rounded-lg shadow-md min-h-[400px] flex flex-col">
                         <img src={carpoolImage} alt="Project 2" className="rounded-lg mb-4 w-full h-48 object-cover" />
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import githubimage from '../components/images/githublogo.png';
 
 const Projects = () => {
     return (
@@ -7,57 +8,64 @@ const Projects = () => {
             <div className="grid grid-cols-1 gap-8">
                 <div className="bg-white p-6 rounded-lg shadow-md flex">
                     <div className="flex-1">
-                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">VehicleExpo E-commerce
-                        </h3>
+                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">School Project: VehicleExpo E-commerce</h3>
                         <p className="text-gray-600">A full-stack car merchandise web application. Has all functioning e-commerce components including login, review writing, checkout...etc.</p>
                     </div>
                     <div className="ml-4">
-                        <p className="text-gray-600">MongoDB,Express.js,React,Node.js</p>
-                        {/* You can add images or additional content here */}
+                        <p className="text-gray-600">MongoDB, Express.js, React, Node.js</p>
                     </div>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md flex">
                     <div className="flex-1">
-                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">Footwear Shopping Simulator</h3>
-                        <p className="text-gray-600">A front-end project that simulates the online footwear shopping, was developed to perform the study comparing task completion speed of labeled vs unlabeled UI components</p>
+                <div className="flex items-center">
+                <h3 className="text-2xl font-semibold mb-2 text-gray-700">School Project: Footwear Shopping Simulator</h3>
+                <a href="https://github.com/KyongRok/Lima-Project4443" target="_blank" rel="noopener noreferrer" className="ml-2">
+                    <img src={githubimage} alt="GitHub" className="h-6 w-6" />
+            </a>
+        </div>
+        <p className="text-gray-600">An Android mobile app that simulates the online footwear shopping, was developed to perform the study comparing task completion speed of labeled vs unlabeled UI components.</p>
+    </div>
+    <div className="ml-4">
+        <p className="text-gray-600">Android Studio, Java, SQLite</p>
+    </div>
+</div>
+                <div className="bg-white p-6 rounded-lg shadow-md flex">
+                    <div className="flex-1">
+                        <div className="flex items-center">
+                            <h3 className="text-2xl font-semibold mb-2 text-gray-700">Movie Search App</h3>
+                            <a href="https://github.com/seankimfeb/moviesearch" target="_blank" rel="noopener noreferrer" className="ml-2">
+                                <img src={githubimage} alt="GitHub" className="h-6 w-6" />
+                            </a>
+                        </div>
+                        <p className="text-gray-600">Web application that makes use of OMDb API to allow users to search movie titles and acquire movie info such as poster, release year, cast, and more.</p>
                     </div>
                     <div className="ml-4">
-                        <p className="text-gray-600">Android Studio, Java, SQLite</p>
-                        {/* You can add images or additional content here */}
+                        <p className="text-gray-600">Java, Spring Boot</p>
                     </div>
                 </div>
                 <div className="bg-white p-6 rounded-lg shadow-md flex">
                     <div className="flex-1">
-                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">Movie Search App</h3>
-                        <p className="text-gray-600">Web application that makes use of OMDb API to allow users to search movie titles and acquire movie info such as poster, release year, cast, and more</p>
-                    </div>
-                    <div className="ml-4">
-                        <p className="text-gray-600"> Java, Spring Boot</p>
-                        {/* You can add images or additional content here */}
-                    </div>
-                </div>
-                <div className="bg-white p-6 rounded-lg shadow-md flex">
-                    <div className="flex-1">
-                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">Carpool Application</h3>
+                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">Hackathon: Carpool Application</h3>
                         <p className="text-gray-600">A mobile app that allows users to carpool and calculates how much cost of travel each person owes to the driver.</p>
                     </div>
                     <div className="ml-4">
                         <p className="text-gray-600">MS Power Apps</p>
-                        {/* You can add images or additional content here */}
                     </div>
                 </div>
-                
                 <div className="bg-white p-6 rounded-lg shadow-md flex">
-                    <div className="flex-1">
-                        <h3 className="text-2xl font-semibold mb-2 text-gray-700">Course Enrollment Simulator</h3>
-                        <p className="text-gray-600">Script which simulates course enrollment using command prompt. Has a feature to search courses, enroll, de-enroll, and sort students by their ages</p>
-                    </div>
-                    <div className="ml-4">
-                        <p className="text-gray-600">C</p>
-                        {/* You can add images or additional content here */}
-                    </div>
-                </div>
-                {/* Add more divs for additional projects */}
+    <div className="flex-1">
+        <div className="flex items-center">
+            <h3 className="text-2xl font-semibold mb-2 text-gray-700">Data Format Converter</h3>
+            <a href="https://github.com/seankimfeb/moviesearch" target="_blank" rel="noopener noreferrer" className="ml-2">
+                <img src={githubimage} alt="GitHub" className="h-6 w-6" />
+            </a>
+        </div>
+        <p className="text-gray-600">A Python app that allows users to convert data files to other formats, such as XML to JSON or vice versa.</p>
+    </div>
+    <div className="ml-4">
+        <p className="text-gray-600">Python (Tkinter, Pandas)</p>
+    </div>
+</div>
 
             </div>
         </div>
